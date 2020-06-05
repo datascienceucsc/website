05233cbdfd015da70f0abc38061d698fee69a425
Sean Breckenridge <seanbrecke@gmail.com>
2020-06-05 13:44:46 -0700

    sync typescript css type

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare module "*.css" {
+  interface IClassNames {
+    [className: string]: string;
+  }
+  const classNames: IClassNames;
+  export = classNames;
+}
+
