09057da75dd8d52ac692442d58b94270897991c9
Sean Breckenridge <seanbrecke@gmail.com>
2020-06-05 11:15:06 -0700

    Updates

---
+++ _next/static/a_mMElRNBN-GwTl0wXahS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/posts/[id]":[a,b]}}("static\u002Fchunks\u002F27954ce19f89a5cf2712de1037055647dff26936.ca09c0c8e0f8774431f5.js","static\u002Fcss\u002Fad9d4b93fec8673a55fa.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
