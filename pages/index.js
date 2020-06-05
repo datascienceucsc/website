ed95de5c42fe6dc1bb83861bd97788a62fdfc961
Sean Breckenridge <seanbrecke@gmail.com>
2020-06-05 12:22:00 -0700

    fix prod URL

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href="/posts/[id]" as={ process.env.BASE_URL + `/posts/${id}` }>
+              <Link href="/posts/[id]" as={ `${process.env.BASE_URL}/posts/${id}` }>
                 <a>{title}</a>
               </Link>
               <br />
