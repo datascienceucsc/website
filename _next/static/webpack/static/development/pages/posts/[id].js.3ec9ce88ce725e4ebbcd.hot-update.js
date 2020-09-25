12a8b34496ec064b7926483c944ca98272d32886
Anders Poirel <andersjopo@gmail.com>
2020-09-25 15:34:40 -0700

    Updates

---
+++ _next/static/webpack/static/development/pages/posts/[id].js.3ec9ce88ce725e4ebbcd.hot-update.js
@@ -0,0 +1,39 @@
+webpackHotUpdate("static/development/pages/posts/[id].js",{
+
+/***/ "./config.tsx":
+/*!********************!*\
+  !*** ./config.tsx ***!
+  \********************/
+/*! exports provided: name, siteTitle, navBar */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "name", function() { return name; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "siteTitle", function() { return siteTitle; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "navBar", function() { return navBar; });
+// defines configuration for the website
+// name of site/title
+var name = "DataScience";
+var siteTitle = "Data Science @ SC";
+// define pages linked in the header
+// these ids correspond to the names of pages (without .md) in posts
+// the Home link is added automatically at the beginning of this list
+var navBar = [{
+  linkText: "About",
+  pageId: "about"
+}, {
+  linkText: "Resources",
+  pageId: "resources"
+}, {
+  linkText: "What is data science?",
+  pageId: "what_is_ds"
+}, {
+  linkText: "Events",
+  pageId: "events"
+}];
+
+/***/ })
+
+})
+//# sourceMappingURL=[id].js.3ec9ce88ce725e4ebbcd.hot-update.js.map
