3314032672195ebe1d2530e1258774f13aaf21c6
Julian Lehrer <jmlehrer@ucsc.edu>
2020-10-06 20:04:01 -0700

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9b650b63e06261583919";
+/******/ 	var hotCurrentHash = "fa58490dc77a58413124";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
